1846070bf6d2b7d5880b33cbcd7f605238b124f4
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-25 10:34:28 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,78 @@
 import streamlit as st
+
+# --- FUNCTIONS ---
+def popup_html(row):
+    
+    i = row
+
+    datum=df_2['datum'].iloc[i] 
+    datum_2=df_2['datum_2'].iloc[i] 
+    functie=df_2['functie'].iloc[i]
+    opmerking=df_2['opmerking'].iloc[i]
+    waarnemer=df_2['waarnemer'].iloc[i] 
+       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum verwijderd</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum_2) + """
+    </tr>
+    <tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
+
+
+def logIn():
+    name = st.text_input("Vul uw gebruikersnaam in, alstublieft",value=None)  
+    password = st.text_input("Vul uw wachtwoord in, alstublieft")
+    try:
+        if name == None:
+            st.stop()
+        
+        index = df_references[df_references['username']==name].index[0]
+        true_password = df_references.loc[index,"password"]
+
+    except:
+        st.warning("De gebruikersnaam is niet correct.")
+        st.stop()
+                             
+    if st.button("logIn"):
+        if password == true_password:
+            st.session_state.login = {"name": name, "password": password}
+            st.rerun()
+
+        else:
+            st.markdown(f"Sorry {name.split()[0]}, het wachtwoord is niet correct.")
+
+
+def logOut():
+    if st.button("logOut",use_container_width=True):
+        del st.session_state.login
+        # del st.session_state.project     
+        st.rerun()
